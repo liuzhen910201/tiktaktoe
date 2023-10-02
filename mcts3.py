2edbfac9730ed97f0bb0d335ec4e0d3c55fdacd3
lz910201 <lz910201@gmail.com>
2023-10-02 13:03:20 +0900

    0~8→1~9

--- mcts3.py
+++ mcts3.py
@@ -9,11 +9,12 @@ class TicTacToe:
 
     def print_board(self):
         for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' |')
+            print('| ' + ' | '.join(map(lambda x: str(x) if x != ' ' else ' ', row)) + ' |')
+
 
     @staticmethod
     def print_board_nums():
-        number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
+        number_board = [[str(i+1) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
@@ -64,9 +65,9 @@ class HumanPlayer:
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.symbol + '\'s turn. Input move (0-8): ')
+            square = input(self.symbol + '\'s turn. Input move (1-9): ')
             try:
-                val = int(square)
+                val = int(square) - 1
                 if val not in game.available_moves():
                     raise ValueError
                 valid_square = True
@@ -92,7 +93,7 @@ class MonteCarloPlayer:
             return random.choice(game.available_moves())
 
         # 调整检索次数
-        simulations = 1000
+        simulations = 200
         square = self.monte_carlo_tree_search(game, simulations)
         return square
 
@@ -111,7 +112,6 @@ class MonteCarloPlayer:
         return best_move
 
 
-
 def play(game, x_player, o_player, print_game=True):
     if print_game:
         game.print_board_nums()
@@ -125,7 +125,7 @@ def play(game, x_player, o_player, print_game=True):
 
         if game.make_move(square, letter):
             if print_game:
-                print(letter + f' makes a move to square {square}')
+                print(letter + f' makes a move to square {square+1}')
                 game.print_board()
                 print('')  # empty line
 
@@ -148,4 +148,3 @@ if __name__ == '__main__':
     o_player = MonteCarloPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
-
